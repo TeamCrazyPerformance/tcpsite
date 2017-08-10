568be47e5846cb20960774135b2ae06d8e6cbe1d
blessldk <lovmistake@gmail.com>
2017-08-10 23:07:50 +0900

    [create] init project

---
+++ src/main/java/com/tcp/home/api/TcpHomeApiServerApplication.java
@@ -0,0 +1,12 @@
+package com.tcp.home.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcpHomeApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcpHomeApiServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tcp/home/api/TcpHomeApiServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tcp.home.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TcpHomeApiServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
